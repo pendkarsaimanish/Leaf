44388852aceda73f6343a46bbc7788e8c1a51e41
pendkarsaimanish <pendkarsaimanish@outlook.com>
2025-07-08 00:58:26 +0530

    Added a minimal UI for profile page

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,4 +1,6 @@
 import { useAuth } from "../context/AuthContext";
+import IconButton from "@mui/joy/IconButton";
+import EditIcon from "@mui/icons-material/Edit";
 
 export const ProfilePage = () => {
   const { user } = useAuth();
@@ -7,8 +9,93 @@ export const ProfilePage = () => {
 
   return (
     <>
-      <p>Name {user.name}</p>
-      <p>Email {user.email}</p>
+      <div className="flex min-h-full flex-1 flex-col px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm"></div>
+
+        <div className="">
+          <div className="space-y-6">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Name
+              </label>
+              <div className="flex space-x-2 mt-2">
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  required
+                  autoComplete="name"
+                  value={user?.name}
+                  readOnly
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+                <IconButton>
+                  <EditIcon />
+                </IconButton>
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Email address
+              </label>
+
+              <div className="flex space-x-2 mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  autoComplete="email"
+                  value={user?.email}
+                  readOnly
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+                <IconButton>
+                  <EditIcon />
+                </IconButton>
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm/6 font-medium text-gray-900"
+                >
+                  Password
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  autoComplete="current-password"
+                  readOnly
+                  value={"********"}
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Save
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
